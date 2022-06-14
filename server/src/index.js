de991cb55b6525a9abe4722e711690148d699330
Quentin Rault <q.rault054@gmail.com>
2022-06-14 12:26:00 +0200

    fix hot-reloading

--- server/src/index.js
+++ server/src/index.js
@@ -21,17 +21,17 @@ process.on("SIGTERM", () => {
 
     app.get('/', (req, res) => {
         console.log("Got a request");
-        res.json({ message: "Hey, I'm Tom, the API" });
+        res.json({ message: "Hey, I'm Tom, the API again again" });
     });
 
     app.post('/',  async (req, res) => {
         console.log("Got a post");
         const message = new MessageModel({sentence: "Hey I'm a new post!"});
         const result = await message.save();
-        res.json({ message: "Hey, I saved a post", result });
+        res.json({ message: "Hey, I saved a post now again", result });
     })
 
     app.listen(5000, () => {
-        console.log("Server is running and hot-reloads");
+        console.log("Server is running and hot-reloads.");
     });
 })();
